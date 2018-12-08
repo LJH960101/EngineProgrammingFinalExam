fb51844fd4babcd65785ab4c97df8e17d80781c4
JangWoo <ljh960101@gmail.com>
2018-12-09 05:12:57 +0900

    소스 정리

--- Source/MyGame/GameEssencil/MyGameModeBase.h
+++ Source/MyGame/GameEssencil/MyGameModeBase.h
@@ -13,5 +13,6 @@ UCLASS()
 class MYGAME_API AMyGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+public:
+	AMyGameModeBase();
 };
--- Source/MyGame/MyGame.Build.cs
+++ Source/MyGame/MyGame.Build.cs
@@ -8,7 +8,7 @@ public class MyGame : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "Slate", "SlateCore"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "Slate", "SlateCore", "NavigationSystem"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
